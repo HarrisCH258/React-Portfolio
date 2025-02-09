2bab0dd16fddca3efcfa00113dcbfe4082a64a4f
CH258 <ch2953har@gmail.com>
2025-02-09 13:41:14 -0500

    add pages and styles

---
+++ vite-project/src/components/Content.jsx
@@ -0,0 +1,19 @@
+import '../styles/Content-styles.css';
+import React from 'react';
+
+function Content(props) {
+    return (
+        <a href={props.url} target="_blank" rel="noopener noreferrer" className="project-link">
+        <div className="project-container">
+            <div className="image-container">
+                <img src={props.image} alt={props.title} className="image" />
+                <div className="middle">
+                    <div className="text">{props.title}</div>
+                </div>
+            </div>
+        </div>
+        </a>
+    );
+};
+
+export default Content;
