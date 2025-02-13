94dbbc0ee25a5e5ad1b09b325c3b5f634ee19d28
CH258 <ch2953har@gmail.com>
2025-02-13 10:40:18 -0500

    add final touches

--- vite-project/src/pages/aboutMe.jsx
+++ vite-project/src/pages/aboutMe.jsx
@@ -6,7 +6,7 @@ function About() {
         <div class='about'>
             <h1><strong>About Me</strong></h1>
             <div class='about-content'>
-            <img class='ch' src='/assets/Chad.jpg' alt='Chad Harris' />
+            <img class='ch' src='../assets/Chad.jpg' alt='Chad Harris' />
             <p class='me'>
                 I am a full stack web developer with a passion for creating and designing. I have experience in HTML, CSS, JavaScript, and React. I am currently learning Node.js and Express.js. I am a recent graduate of The Ohio State University Coding Bootcamp. I am excited to continue learning and growing in the tech industry. I am a hard worker and a quick learner. I am excited to see where my career in web development takes me.
             </p>
